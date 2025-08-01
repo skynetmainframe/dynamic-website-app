83873c3be5acea940e8c8f248d1f8a652964caab
skynetmainframe <157806410+skynetmainframe@users.noreply.github.com>
2025-08-01 11:17:03 +0200

    Update MusicAPIInfo.java

--- backend/src/main/java/com/example/dynamicwebsitebackend/model/MusicAPIInfo.java
+++ backend/src/main/java/com/example/dynamicwebsitebackend/model/MusicAPIInfo.java
@@ -1,1 +1,13 @@
+package com.example.dynamicwebsitebackend.model;
 
+public class MusicAPIInfo {
+    private String name;
+    private String description;
+
+    // Getters and setters, and constructors
+    public MusicAPIInfo(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+    // ...
+}
